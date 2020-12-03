b2ca2c4e0e2a381ada38d5aad4e7d74142a53afc
Khamparia1988 <55939148+Khamparia1988@users.noreply.github.com>
2020-12-03 10:21:14 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -6,30 +6,30 @@ window.onload = function (e) {
           <div class="mb-0 text-center col-12 col-sm col-md">
             <span id="jumbo-images1">
 
-           <i id="one"><img src="gallery/sdu_logo.jpg" alt="sdulogo" width="200" height="200" hspace= "15" class="img-fluid sponsor_image">    </i>
+           <i id="one"><img src="gallery/PIET.jfif" alt="PIET" width="200" height="200" hspace= "15" class="img-fluid sponsor_image">    </i>
 
-            <i id="two"><img src="gallery/isparta.jpg" alt="isparta" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>  <br><br>
-          <i id="two"><img src="gallery/COER.jpg" alt="COER" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>
+        <!--    <i id="two"><img src="gallery/isparta.jpg" alt="isparta" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>  <br><br>
+          <i id="two"><img src="gallery/COER.jpg" alt="COER" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>-->
             </span>
           </div>
 
           <div class="col-12 col-sm col-md mb-0 text-center">
 
-            <h1 style="font-size: x-large">1<sup>st</sup> INTERNATIONAL CONFERENCE ON COMPUTATIONAL RESEARCH AND DATA ANALYTICS</h1>
-            <h1 style="font-size: x-large">ICCRDA-2020</h1>
+            <h1 style="font-size: x-large">1<sup>st</sup> 2<sup>nd</sup> INTERNATIONAL CONFERENCE ON INTELIGENT COMMUNICATION AND COMPUTATIONAL RESEARCH </h1>
+            <h1 style="font-size: x-large">ICICCR-2021</h1>
 
-            <h2 style="font-size:1.4rem";text-transform:uppercase">Jointly Organized by:  Suleyman Demirel University, Turkey & Isparta Applied Sciences University, Turkey [INTERNATIONAL] and College of Engineering Roorkee, India </h2>
-             <h2 style="font-size:1.4rem">24<sup>th</sup> October 2020</h2>
-            <h3 style="font-size:1.2rem;text-transform:uppercase"> In Association With </h3>
+            <h2 style="font-size:1.4rem";text-transform:uppercase">Organized by:  Punjab Institute of Engineering and Technology, Haryana, India </h2>
+             <h2 style="font-size:1.4rem">30<sup>th</sup> April 2021</h2>
+           <1-- <h3 style="font-size:1.2rem;text-transform:uppercase"> In Association With </h3>
 
             <hr id="hrr">
-            <h3 style="font-size:1.2rem;text-transform:uppercase"> Intelligent Communication and Computational Research Lab (ICCRLAB)</h3>
+            <h3 style="font-size:1.2rem;text-transform:uppercase"> Intelligent Communication and Computational Research Lab (ICCRLAB)</h3>-->
             <p id="demo" style="font-size:1.6rem"></p>
           </div>
           <div class="col-12 col-sm text-center">
 
-            <img src="gallery/PicsArt_03-03-04.47.03.png" alt="" width="200" height="200">
-            <img src="proceeding_partners/IOP NEW LOGO.png" alt="MSE" class="img-fluid sponsor_image">
+         <!--   <img src="gallery/PicsArt_03-03-04.47.03.png" alt="" width="200" height="200">-->
+            <img src="gallery/Springer.png" alt="MSE" class="img-fluid sponsor_image">
           </div>
         </div>
 
@@ -53,7 +53,7 @@ window.onload = function (e) {
                         <br>
                         <i class="fa fa-phone"> </i> &nbsp;: +<br>
                         <i class="fa fa-fax"> </i> : +<br>
-                        <i class="fa fa-envelope"> </i> : <a href="mailto:icicc.editor@gmail.com">icicc.editor@gmail.com</a>
+                        <i class="fa fa-envelope"> </i> : <a href="mailto:iciccr.editor@gmail.com">iciccr.editor@gmail.com</a>
                      </address>
                       </div>
                       <div class="col-12 col-sm-4 align-self-center">
@@ -70,7 +70,7 @@ window.onload = function (e) {
                  <div class="row justify-content-center">
                       <div class="col-auto">
                         <section class="sec Copyright">
-                          <p class="footer_p_i_h2_a footer_p_i_a">Copyright © Intelligent Communication and Computational Research Laboratory| All Rights Reserved | <a href="https://www.linkedin.com/in/sunnam-sai-jyothi-swaroop" target="_blank" style="color: teal;cursor: pointer;">Website Developed By S Sai Jyothi Swaroop</a> <a style="color: teal;cursor: pointer;" href="https://www.linkedin.com/in/manavarthi-venkat-rao?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bi%2B9dH7EqRVKLiciXidLBRg%3D%3D" target="_blank">and M Venkat Rao </a><a href="https://www.linkedin.com/in/karan-singh227" target="_blank" style="color: teal;cursor: pointer;">and Karan Singh</a>|</p>
+                          <p class="footer_p_i_h2_a footer_p_i_a">Copyright © Intelligent Communication and Computational Research| All Rights Reserved | <a href="https://www.linkedin.com/in/sunnam-sai-jyothi-swaroop" target="_blank" style="color: teal;cursor: pointer;">Website Developed By S Sai Jyothi Swaroop</a> <a style="color: teal;cursor: pointer;" href="https://www.linkedin.com/in/manavarthi-venkat-rao?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bi%2B9dH7EqRVKLiciXidLBRg%3D%3D" target="_blank">and M Venkat Rao </a><a href="https://www.linkedin.com/in/karan-singh227" target="_blank" style="color: teal;cursor: pointer;">and Karan Singh</a>|</p>
                   </section>
                       </div>
                  </div>
