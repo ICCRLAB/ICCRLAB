6a666b4d6348d6bde266b48251eeb80f192235b0
Khamparia1988 <55939148+Khamparia1988@users.noreply.github.com>
2020-12-04 11:57:31 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -143,7 +143,7 @@ window.onload = function (e) {
                       </li>
 
                       <li class="nav-item">
-                        <a class="nav-link text-white" href="spon_sorship.html">Sponsorship</a>
+                        <a class="nav-link text-white" href="spon_sorship.html">Previous Conferences</a>
                       </li>
                       
                       <li class="nav-item">
