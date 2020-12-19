6f5683d18c999baa6781aa573ccc9b47a68dcc1c
Khamparia1988 <55939148+Khamparia1988@users.noreply.github.com>
2020-12-19 23:16:03 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -28,7 +28,7 @@ window.onload = function (e) {
           </div>
           <div class="col-12 col-sm text-center">
 
-           <img src="gallery/Springer.png" alt="" width="200" height="200">
+         <!--  <img src="gallery/Springer.png" alt="" width="200" height="200">-->
            <!-- <img src="gallery/Springer.png" alt="MSE" class="img-fluid sponsor_image">-->
           </div>
           <marquee> <h2 style="font-size:1.4rem">ICICCR-2021 moved from Physical Mode to Digital Mode due to Pandemic COVID 19.</h2></marquee>
