4a333df602edcd94ee29262d9bd057a1b8f7bb2a
Khamparia1988 <55939148+Khamparia1988@users.noreply.github.com>
2021-01-28 11:41:24 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -8,7 +8,7 @@ window.onload = function (e) {
 
            <i id="one"><img src="ICCRLAB.github.io/gallery/BSF1.jpeg" alt="PIET" width="250" height="250" hspace= "15" class="sponsor_image">    </i>
 
-      <!--     <i id="two"><img src="gallery/BSF2.jpeg" alt="isparta" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>  <br><br>-->
+          <i id="two"><img src="gallery/AIP.jfif" alt="isparta" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>  <br><br>
       <!--    <i id="two"><img src="gallery/COER.jpg" alt="COER" width="200" height="200" hspace= "10" class="img-fluid sponsor_image"></i>-->
             </span>
           </div>
@@ -132,7 +132,7 @@ window.onload = function (e) {
                         </div>
                       </li>
 
-                   <!--   <li class="nav-item">
+                      <li class="nav-item">
                         <a class="nav-link text-white " href="conference_venue.html">Conference Venue</a>
                       </li>
                       <li class="nav-item">
@@ -140,7 +140,7 @@ window.onload = function (e) {
                       </li>
                       <li class="nav-item">
                         <a class="nav-link text-white" href="Gallary.html">Gallery</a>
-                      </li>-->
+                      </li>
 
                    <!--   <li class="nav-item dropdown dmenu">
                             <a class="nav-link dropdown-toggle text-white" href="#" id="navbardrop" data-toggle="dropdown">
