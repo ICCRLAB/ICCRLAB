3189302c7a7dc3615cb02714ac8145dfe43faecd
Khamparia1988 <55939148+Khamparia1988@users.noreply.github.com>
2021-05-08 15:32:38 +0530

    Update main-js.js

--- js/main-js.js
+++ js/main-js.js
@@ -195,7 +195,7 @@ window.onload = function (e) {
     });
   }
   // Set the date we're counting down to
-var countDownDate = new Date("May 30, 2021 12:00:00").getTime();
+var countDownDate = new Date("July 25, 2021 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
